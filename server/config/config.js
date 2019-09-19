9a34d51976cd58f40e6e752e3c6c5852eae632e1
Alex Dementev <demsasha4yt@yandex.ru>
2019-09-19 04:15:16 +0300

    Added installDepsScript

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  source: 'http://145.239.133.5/servers.json',
+  source: process.env.SANTROPE ||'http://145.239.133.5/servers.json',
   interval: 1000 * 60 * 10,
-  port: 3000,
+  port: process.env.PORT || 3000,
   db: {
     database: process.env.DB_NAME || 'uROkPuw2iQ',
     user: process.env.DB_USER || 'uROkPuw2iQ',
