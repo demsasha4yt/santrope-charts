69fa58087b97192694598de439461cbaabd000b4
Alex Dementev <demsasha4yt@yandex.ru>
2019-09-24 18:48:25 +0300

    Optimized graph

--- client/src/components/amChartOnline.vue
+++ client/src/components/amChartOnline.vue
@@ -13,6 +13,10 @@ import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import OnlineService from '@/services/OnlineService'
 am4core.useTheme(am4themes_animated);
+am4core.options.queue = true;
+am4core.options.minPolylineStep = 5;
+am4core.options.onlyShowOnViewport = true;
+
 export default {
   name: 'HelloWorld',
   data() {
@@ -26,33 +30,32 @@ export default {
   },
   methods: {
     createChart() {
-      am4core.options.minPolylineStep = 5;
       let chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
-      chart.responsive.enabled = true;
-      chart.data = this.onlineData.reverse()
-      chart.paddingRight = 20;
       let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
       categoryAxis.dataFields.category = "createdAt"
-      categoryAxis.renderer.inside = true;
+
       let label = categoryAxis.renderer.labels.template;
       label.wrap = true;
       label.maxWidth = 120;
+
       let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
       valueAxis.tooltip.disabled = true;
       valueAxis.renderer.minWidth = 35;
+
       let series = chart.series.push(new am4charts.LineSeries());
       series.minBulletDistance = 20;
       series.dataFields.categoryX = "createdAt";
       series.dataFields.valueY = "players";
       series.tooltipText = "{valueY.value}";
-      chart.cursor = new am4charts.XYCursor();
+     
       let scrollbarX = new am4charts.XYChartScrollbar();
       scrollbarX.series.push(series);
       scrollbarX.parent = chart.bottomAxesContainer;
+
+      chart.responsive.enabled = true;
+      chart.data = this.onlineData.reverse()
+      chart.cursor = new am4charts.XYCursor();
       chart.scrollbarX = scrollbarX;
-      
-      this.chart = chart;
-      // console.log(this.chart.data)/
     },
     update() {
         OnlineService.online()
